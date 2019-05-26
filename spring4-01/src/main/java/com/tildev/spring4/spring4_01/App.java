ebe9fe7a132a5147f80e11ae5d3cf165e81ef2ad
tildev <tildevelove@gmail.com>
2019-05-26 22:15:37 +0900

    add maven project

---
+++ spring4-01/src/main/java/com/tildev/spring4/spring4_01/App.java
@@ -0,0 +1,13 @@
+package com.tildev.spring4.spring4_01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
