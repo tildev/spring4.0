9405748c3d66bab8970ef5d07a2e47c78de36ee5
tildev <tildevelove@gmail.com>
2019-05-26 22:25:30 +0900

    edit pom.xml and apply code formater

--- spring4-01/src/main/java/com/tildev/spring4/spring4_01/App.java
+++ spring4-01/src/main/java/com/tildev/spring4/spring4_01/App.java
@@ -4,10 +4,8 @@ package com.tildev.spring4.spring4_01;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
